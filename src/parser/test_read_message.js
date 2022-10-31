52bb26237876de0b718584b25092286170d450b6
nchappell_mono <nchappell@mono-software.com>
2022-10-31 14:12:25 +0100

    string read test

--- src/parser/test_read_message.js
+++ src/parser/test_read_message.js
@@ -44,4 +44,45 @@ function test_readMessages() {
     })
 }
 
+function test_readMessages_string() {
+    const tree = makeParseInfoTree([
+        "A1: Byte1('A') Int32 Int32(0) Byte[]",
+        "A2: Byte1('A') Int32 Int32(1) String",
+    ])
+
+    // console.log(JSON.stringify(tree, null, 2))
+
+    const message = [
+        { type: "Int32", value: 1 },
+        { type: "String", value: 'foobar' },
+    ]
+
+    /** @type {Readable} */
+    let readable
+
+    const writable = new Writable({
+        objectMode: false,
+        write: (data, encoding, cb) => {
+            readable = Readable.from([data], { objectMode: false })
+            // console.log(` readMessage: ${data.toString('hex')}`)
+            cb()
+        }
+    })
+    writeMessage(writable, 'A', message).then(async () => {
+        const gen = readMessages(readable, tree)
+        const { value } = await gen.next()
+        // console.log(JSON.stringify(value,null,2))
+        const messageType = value.messageType
+        const expectedMessageType = 'A2'
+        const typeSequence = value.message.map(v => v.type).join(',')
+        const expectedTypeSequence = 'Byte1,Int32,Int32,String'
+        const expectedStringValue = 'foobar';
+        const stringValue = value.message[3].value;
+        if (messageType !== expectedMessageType) throw new Error(`Expected messageType ${expectedMessageType}, got ${messageType}`)
+        if (typeSequence !== expectedTypeSequence) throw new Error(`Expected typeSequence ${expectedTypeSequence}, got ${typeSequence}`)
+        if (stringValue !== expectedStringValue) throw new Error(`Expected stringValue ${expectedStringValue}, got ${stringValue}`)
+    })
+}
+
 test_readMessages()
+test_readMessages_string()
