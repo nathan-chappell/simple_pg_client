d9b3a1d1f9cdaab6006046e0206709cfd27b5be6
DESKTOP-LPKA7MV\natha <nathan.s.chappell@gmail.com>
2022-11-09 23:01:04 +0100

    string[]

--- src/codegen/ITV.ts
+++ src/codegen/ITV.ts
@@ -1,9 +1,13 @@
 export interface ITV {
     name: string
-    value: string | number | number[] | ITV[]
+    value: string | string[] | number | number[] | ITV[]
 }
 
-export type ITVObject = { [name: string]: string | number | number[] | ITVObject }
+export type ITVObject = { [name: string]: string | string[] | number | number[] | ITVObject }
+
+export function isStringArray(a: unknown): a is ITV[] {
+    return Array.isArray(a) && typeof a[0] === 'string'
+}
 
 export function isNumberArray(a: unknown): a is number[] {
     return Array.isArray(a) && typeof a[0] === 'number'
